56f77eaca7afd1ff7f40793fe3c618778f1f1818
Mando <armando@seekirt.io>
2019-11-04 19:37:11 -0800

    started first iteration of homebrew

---
+++ seekrit.rb
@@ -0,0 +1,13 @@
+class Weather < Formula
+  desc "A command line tool to retrieve local weather"
+  homepage "https://seekrit.io/"
+  url "https://homebrew-seekrit.s3.us-east-2.amazonaws.com/seekrit.tar.gz"
+  sha256 "e1ef675fbea7f96563ac0004fa465cec7346665b2365e318968675cb50650862"
+  version "0.7.0"
+
+  bottle :unneeded
+
+  def install
+    bin.install "seekrit"
+  end
+end
