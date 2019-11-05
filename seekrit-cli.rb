7d16acaa88b9fe122de10425b00bd13c15f4f4e4
Mando <armando@seekirt.io>
2019-11-04 21:58:28 -0800

    Updated version

--- seekrit-cli.rb
+++ seekrit-cli.rb
@@ -1,7 +1,7 @@
 class SeekritCli < Formula
   desc "A command line tool to retrieve local weather"
   homepage "https://seekrit.io/"
-  url "https://homebrew-seekrit.s3.us-east-2.amazonaws.com/seekrit.tar.gz"
+  url "https://homebrew-seekrit.s3.us-east-2.amazonaws.com/seekrit-0.7.0.tar.gz"
   sha256 "e1ef675fbea7f96563ac0004fa465cec7346665b2365e318968675cb50650862"
   version "0.7.0"
 
